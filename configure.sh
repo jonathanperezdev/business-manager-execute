87c318718cde0eab5ef5a89aea700e0aca98de2f
jonathan.perez <jonathan.perez@globant.com>
2020-08-06 14:00:17 -0500

    Added horario scripts

--- configure.sh
+++ configure.sh
@@ -1,7 +1,13 @@
-export BM_HOME=/home/jonathanperez/java/personal/business-manager
+export BM_HOME=/home/jonathanperez/Documents/personal/intellij_projects/business-manager
+
+#Backend
 sudo ln -s $BM_HOME/backend/business-manager-cloud $BM_HOME/business-manager-execute/cloud
 sudo ln -s $BM_HOME/backend/business-manager-empleado $BM_HOME/business-manager-execute/empleado
+sudo ln -s $BM_HOME/backend/business-manager-horario $BM_HOME/business-manager-execute/horario
+
+#FrontEnd
 sudo ln -s $BM_HOME/frontend/business-manager-empleado-ui $BM_HOME/business-manager-execute/empleado-ui
+sudo ln -s $BM_HOME/frontend/business-manager-horario-ui $BM_HOME/business-manager-execute/horario-ui
 
 echo "Script executed with BM_HOME in $BM_HOME"
 
--- execute.sh
+++ execute.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
-echo "use -u flag to up and -d to down all the services"
+echo "use -u flag to up -b to build and up and -d to down all the services"
 
-while getopts ":ud" OPTION; do
+while getopts ":udb" OPTION; do
     case $OPTION in
     u)
 	ACTION='starting services'
-	sudo docker network create BM_NETWORK
-    	sudo docker-compose -f ./empleado-ui/docker-compose.yml up -d
-    	sudo docker-compose -f ./cloud/docker-compose.yml up -d
-    	sudo docker-compose -f ./empleado/docker-compose.yml up -d
+        sh configure.sh
+
+        #Network
+	    docker network create BM_NETWORK    	
+        
+        #Cloud
+        docker-compose -f ./cloud/docker-compose.yml up -d
+
+        #Empleado
+        docker-compose -f ./empleado-ui/docker-compose.yml up -d
+        docker-compose -f ./empleado/docker-compose.yml up -d
+        
+        #Horario
+        docker-compose -f ./horario-ui/docker-compose.yml up -d
+        docker-compose -f ./horario/docker-compose.yml up -d    	
         ;;
+    b)
+	ACTION='build and deploy services'
+        sh configure.sh
+
+        #Network
+	    docker network create BM_NETWORK    	
+        
+        #Cloud
+        mvn clean install -f ./cloud
+        docker-compose -f ./cloud/docker-compose.yml up -d
+
+        #Empleado
+        yarn --cwd ./empleado-ui build
+        docker-compose -f ./empleado-ui/docker-compose.yml up -d --build
+
+        mvn clean install -f ./empleado
+        docker-compose -f ./empleado/docker-compose.yml up -d
+        
+        #Horario
+        yarn --cwd ./horario-ui build
+        docker-compose -f ./horario-ui/docker-compose.yml up -d --build
+
+        mvn clean install -f ./horario
+        docker-compose -f ./horario/docker-compose.yml up -d
+        ;; 
     d)
 	ACTION='stopping services'
-    	sudo docker-compose -f ./empleado-ui/docker-compose.yml down
-    	sudo docker-compose -f ./cloud/docker-compose.yml down
-    	sudo docker-compose -f ./empleado/docker-compose.yml down
-        ;;
+    	docker-compose -f ./empleado-ui/docker-compose.yml down
+        docker-compose -f ./horario-ui/docker-compose.yml down
+    	docker-compose -f ./cloud/docker-compose.yml down
+    	docker-compose -f ./empleado/docker-compose.yml down
+        docker-compose -f ./horario/docker-compose.yml down
+        ;;    
     *)
         echo "Incorrect options provided"
         exit 1
